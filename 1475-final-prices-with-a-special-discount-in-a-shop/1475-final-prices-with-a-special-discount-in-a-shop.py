b4bf75eca6bc54343f18f220adc8ca98ab568983
devnp2007 <devnp2007@gmail.com>
2026-08-11 17:02:46 +0530

    Time: 5 ms (7.68%), Space: 19.3 MB (30.96%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,10 +1,12 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        n = len(prices)
-        result = prices[:] # all elments from begginng to end, basically copything the original price list
-        for i in range(n):
-            for j in range(i+1,n):
-                if prices[j] <= prices[i]:
-                    result[i] = prices[i] - prices[j]
-                    break
-        return result
+        n=len(prices)
+        stack=[n-1]
+        ans=[x for x in prices]
+        for i in range(n-2, -1, -1):
+            while stack and prices[i]<prices[stack[-1]]:
+                stack.pop()
+            if stack: ans[i]-=prices[stack[-1]]
+            stack.append(i)
+        return ans
+        
