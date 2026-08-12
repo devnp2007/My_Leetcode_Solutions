d9b9bfb6d0e214f15301d673fd2c139c74fa9232
devnp2007 <devnp2007@gmail.com>
2026-08-12 18:55:49 +0530

    Time: 0 ms (100%), Space: 31.7 MB (57.17%) - LeetHub

---
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumSwaps(vector<int>& nums) {
+        int l = 0, r = nums.size()-1, ans = 0;
+        while(l<r){
+            if(nums[r] == 0){
+                r--;
+            }else if(nums[l] == 0 && nums[r] != 0){
+                ans++;
+                l++, r--;
+            }else l++;
+        }
+        return ans;
+    }
+};
